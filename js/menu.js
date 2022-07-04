7abd2e1f039bf61199054dec8060a9d380e32976
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-07-04 19:59:03 +0300

    adaptive

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
     body: document.body,
-    openMenuBtn: document.querySelector(".menu-btn-mob"),
+    openMenuBtn: document.querySelector(".menu-mob__open-btn"),
     closeMenuBtn: document.querySelector(".menu-mob__close-btn"),
     menu: document.querySelector(".menu-mob"),
   };
